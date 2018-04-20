f65ced3f913cb4c1607cbc358eb953b191ea0609
Vladimir Rybalko <vladimir.a.rybalko@gmail.com>
2018-04-20 18:29:24 +0200

    Improved way to get currency rates. Add supporting all currencies from fixer.io

---
+++ src/Wox.Plugin.Currency/CurrencyHelper.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Wox.Plugin.Currency
+{
+    public static class CurrencyHelper
+    {
+        private const string Endpoint = @"https://api.fixer.io/latest";
+
+        private static readonly Dictionary<CurrencyPair, double> _cache;
+        private static readonly WebClient _webClient; 
+
+
+        static CurrencyHelper()
+        {
+            _cache = new Dictionary<CurrencyPair, double>();
+
+            _webClient = new WebClient();
+            _webClient.Headers.Add("content-type", "application/json");
+        }
+
+        public static double Convert(string from, string to, double value)
+        {
+            var pair = new CurrencyPair(from, to);
+
+            if (!_cache.ContainsKey(pair))
+            {
+                _cache.Add(pair, GetRate(from, to));
+            }
+
+            var rate = _cache[pair];
+            return rate * value;
+        }
+
+        private static double GetRate(string from, string to)
+        {
+            var endpoint = $"{Endpoint}?base={from.ToUpper()}&symbols={to.ToUpper()}";
+            var result = _webClient.DownloadString(endpoint);
+            var rate = JsonConvert.DeserializeObject<CurrencyRate>(result);
+
+            return rate.Rates.FirstOrDefault().Value;
+        }
+    }
+
+    #region Exchange currencies stuff
+    internal struct CurrencyPair
+    {
+        public CurrencyPair(string from, string to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+
+    internal class CurrencyRate
+    {
+        [JsonProperty(PropertyName = "rates")]
+        public Dictionary<string, double> Rates { get; set; }
+    }
+    #endregion
+}
--- src/Wox.Plugin.Currency/CurrencyPlugin.cs
+++ src/Wox.Plugin.Currency/CurrencyPlugin.cs
@@ -1,154 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Net;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json;
-using Wox.Plugin.Currency.Models;
 
 namespace Wox.Plugin.Currency
 {
     public class CurrencyPlugin : IPlugin
     {
-        #region private fields
-        private PluginInitContext _context;
-        private string localISOSymbol => RegionInfo.CurrentRegion.ISOCurrencySymbol;
-        private readonly Dictionary<SearchParameters,Models.Currency> _cache;  
-        private readonly string oneWaycheckPattern = @"^(\d+(\.\d{1,2})?)?\s([A-Za-z]{3})$"; //10 usd
-        private readonly string twoWaycheckPattern = @"(\d+(\.\d{1,2})?)?\s([A-Za-z]{3})\s([i][n])\s([A-Za-z]{3})"; // 10 usd in nok
-        private string _toCurrency = "";
-        private string _fromCurrency = "";
-        private decimal _money = new decimal();
-        #endregion
-
-        public CurrencyPlugin()
+        private const string Pattern = @"(?<amount>\d+(?:\.\d+)?)\s(?<from>[A-Za-z]+)\s(?:to|in)\s(?<to>[A-Za-z]+)";
+        
+        public void Init(PluginInitContext context)
         {
-            if (_cache == null)
-            {
-                _cache = new Dictionary<SearchParameters, Models.Currency>();
-            }
         }
 
         public List<Result> Query(Query query)
         {
-            var results = new List<Result>();
-            try
+            var match = Regex.Match(query.Search, Pattern);
+            if (match.Success)
             {
-                if (Regex.IsMatch(query.Search, oneWaycheckPattern))
+                var from = match.Groups["from"].Value.ToUpper();
+                var to = match.Groups["to"].Value.ToUpper();
+                var amount = double.Parse(match.Groups["amount"].Value);
+                var value = CurrencyHelper.Convert(from, to, amount);
+
+                return new List<Result>
                 {
-                    if (query.RawQuery != null && query.RawQuery.Split(' ').Length == 2) // 123 usd
+                    new Result
                     {
-                        _money = Convert.ToDecimal(query.FirstSearch);
-                        _toCurrency = localISOSymbol;
-                        _fromCurrency = query.SecondSearch.ToUpper();
-                        //check ISO symbols
-                        if (!Enum.IsDefined(typeof (RateList), query.SecondSearch.ToUpper())) return new List<Result>();
-                        results = LoadCurrency(results);
-                    }
-                    
-                }
-                else if (Regex.IsMatch(query.Search, twoWaycheckPattern))
-                {
-                    if (query.RawQuery != null && query.RawQuery.Split(' ').Length == 4) //123 usd in nok
-                    {
-                        _toCurrency = query.RawQuery.Split(' ')[3].ToUpper();
-                        _fromCurrency = query.SecondSearch.ToUpper();
-                        _money = Convert.ToDecimal(query.FirstSearch);
-                        //check ISO symbols
-                        if (!Enum.IsDefined(typeof(RateList), _fromCurrency)
-                            && !Enum.IsDefined(typeof(RateList), query.RawQuery.Split(' ')[3].ToUpper()))
-                            return results;
-                    results = LoadCurrency(results);
-                    }
-                }
-                else if (query.Search == "currency")
-                {
-                    var ratelist = Enum.GetValues(typeof(RateList));
-                    var rates = "";
-                    foreach (var rate in ratelist)
-                    {
-                        if (rates.Length > 1)
-                        {
-                            rates += $"Avaliable rates are {rate} ";
-                        }
-                        else
-                        {
-                            rates += $"{rate} ,";
-                        }
-                    }
-                    results.Add(new Result
-                    {
-                        Title = rates,
+                        Title = $"{amount} {from} = {value} {to}",
                         IcoPath = "Images/bank.png",
-                        SubTitle = $"Source: fixer.io"
-                    });
-                }
-
-                return results;
-            }
-            catch (Exception)
-            {
-                return new List<Result>();
+                        SubTitle = $"Source: fixer.io."
+                    }
+                };
             }
 
-        }
-
-        private List<Result> LoadCurrency(List<Result> results)
-        {
-            var currency = GetCurrency(new SearchParameters() {BaseIso = _fromCurrency, ToIso = _toCurrency});
-            var rate = currency.GetRate(_toCurrency);
-            results.Add(new Result
-            {
-                Title = $"{_money.ToString()} {_fromCurrency} = {(_money*rate).ToString()} {_toCurrency}",
-                IcoPath = "Images/bank.png",
-                SubTitle = $"Source: fixer.io (Last updated {currency.date})"
-            });
-            return results;
-        }
-
-        public void Init(PluginInitContext context)
-        {
-            _context = context;
-        }
-
-        #region helpers
-        private Models.Currency GetCurrency(SearchParameters searchParameters)
-        {
-            var url = $"http://api.fixer.io/latest?base={searchParameters.BaseIso}&symbols={searchParameters.ToIso}";
-
-            if (_cache.ContainsKey(searchParameters))
-            {
-                if (Convert.ToDateTime(_cache[searchParameters].date) == DateTime.Today)
-                {
-                    return _cache[searchParameters];
-                }
-            }
-
-            if (searchParameters.OptionalIsos != null && searchParameters.OptionalIsos.Any())
-            {
-                url = searchParameters.OptionalIsos.Aggregate(url, (current, optionalIso) => current + $",{optionalIso}");
-            }
-
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-            var response = (HttpWebResponse)request.GetResponse();
-            using (new StreamReader(response.GetResponseStream()))
-            {
-                var responsestring = new StreamReader(response.GetResponseStream()).ReadToEnd();
-
-                var currency =  JsonConvert.DeserializeObject<Models.Currency>(responsestring);
-
-                _cache.Add(searchParameters,currency);
-                return currency;
-            }
-        }
-
-        private bool CheckRegExOnQuery(string query)
-        {
-            return false;
-        }
-        #endregion
+            return new List<Result>();
+        }       
     }
 }
