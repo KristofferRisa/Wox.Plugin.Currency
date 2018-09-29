a3e373ff1daf1d4a244bed616c44d731e04515fc
Kristoffer Risa <kristofferrisa@gmail.com>
2018-09-29 08:42:01 +0200

    Upgraded project files

---
+++ test/Wox.Plugin.Currency.Test/CurrencyPluginTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wox.Core.Plugin;
+
+
+namespace Wox.Plugin.Currency.Test
+{
+    [TestClass]
+    public class CurrencyPluginTest
+    {
+        [TestMethod]
+        public void TestPlugin()
+        {
+            var plugin = new CurrencyPlugin();
+            var q = PluginManager.QueryInit("10 usd in nok");
+
+            //var result = plugin.Query(q);
+        }
+    }
+}
---
+++ test/Wox.Plugin.Currency.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Wox.Plugin.Currency.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wox.Plugin.Currency.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("abaa395b-f66f-43e5-92e7-7af297415e10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
