fd22c498f9849f2b824fe13a42dd5cd979794174
Kristoffer Risa <kristofferrisa@gmail.com>
2023-01-11 21:22:04 +0100

    Started working on v3
    - Updated program flow and logic
    - Fixed issue with currency API
    - Added Settings view

--- src/Wox.Plugin.Currency/CurrencyPlugin.cs
+++ src/Wox.Plugin.Currency/CurrencyPlugin.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -8,126 +7,185 @@ using System.Windows;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using Wox.Plugin.Currency.Models;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using System.Diagnostics;
+using Wox.Plugin.Currency.ViewModels;
+using System.Windows.Controls;
+using Wox.Plugin.Currency.Views;
+using Wox.Infrastructure.Storage;
 
 namespace Wox.Plugin.Currency
 {
-    public class CurrencyPlugin : IPlugin
+    public class CurrencyPlugin : IPlugin, ISavable, ISettingProvider
     {
-        private PluginInitContext _context;
+        private PluginInitContext Context { get; set; }
 
-        private string LocalISOSymbol => RegionInfo.CurrentRegion.ISOCurrencySymbol;
-
-        private readonly Dictionary<SearchParameters, Models.Currency> _cache;
+        private static List<CacheItem> cache;
         
-        private string _toCurrency = "";
+        private static Settings settings;
 
-        private string _fromCurrency = "";
-
-        private decimal _money = new decimal();
+        private static SettingsViewModel viewModel;
 
         public CurrencyPlugin()
         {
-            if (_cache == null)
+            if (cache == null)
             {
-                _cache = new Dictionary<SearchParameters, Models.Currency>();
+                cache = new List<CacheItem>();
             }
         }
 
         public void Init(PluginInitContext context)
         {
-            _context = context;
+            this.Context = context;
+            
+            viewModel = new SettingsViewModel();
+            settings = viewModel.Settings;
         }
 
         public List<Result> Query(Query query)
         {
-            try
+            if (query.FirstSearch == "clear")
             {
-                query
-                    .IsCurrencyConversion()
-                    .IsNotNull();
-
-                _money = Convert.ToDecimal(query.FirstSearch);
-                _fromCurrency = query.SecondSearch.ToUpper();
-                _toCurrency = query.RawQuery.Split(' ')[3].ToUpper() ?? LocalISOSymbol;
-
-                if (!Enum.IsDefined(
-                        typeof(RateList), 
-                        query.RawQuery.Split(' ')[3].ToUpper() 
-                            ?? query.SecondSearch.ToUpper()))
-                    return new List<Result>();
-
-                var searchParameters = new SearchParameters()
+                // Clears old entries 
+                if (cache.Any(x => x.Created.Date < DateTime.Now.Date))
                 {
-                    BaseIso = _fromCurrency,
-                    ToIso = _toCurrency
-                };
-                var currency = GetCurrencyFromApi(searchParameters);
-                var rate = currency.GetRate(_toCurrency);
-                var convertedValue = ((double)(_money * rate)).ToString();
-                return new List<Result>()
-                {   
-                    new Result
-                    {
-                        Title = $"{_money} {_fromCurrency} = {convertedValue} {_toCurrency}",
-                        IcoPath = "Images/bank.png",
-                        SubTitle = $"Enter to copy. Source: https://frankfurter.app (Updated {currency.date})",
-                        Action = c =>
-                        {
-                            try
-                            {
-                                Clipboard.SetText(convertedValue);
-                                return true;
-                            }
-                            catch (ExternalException)
-                            {
-                                MessageBox.Show("Copy failed, please try later");
-                                return false;
-                            }
-                        }
-                    }
-                };
-
-            }
-            catch 
-            {
-                return new List<Result>();
-            }
-
-        }
-             
-        private Models.Currency GetCurrencyFromApi(SearchParameters searchParameters)
-        {
-            var url = $"https://frankfurter.app/latest?base={searchParameters.BaseIso}&symbols={searchParameters.ToIso}";
-
-            if (_cache.ContainsKey(searchParameters))
-            {
-                if (Convert.ToDateTime(_cache[searchParameters].date) == DateTime.Today)
-                {
-                    return _cache[searchParameters];
+                    cache.RemoveAll(x => x.Created.Date < DateTime.Now.Date);
                 }
             }
-
-            if (searchParameters.OptionalIsos != null && searchParameters.OptionalIsos.Any())
+            if (query == null || query.Search.Length < 5)
             {
-                url = searchParameters.OptionalIsos.Aggregate(url, (current, optionalIso) => current + $",{optionalIso}");
-            }
+                return new List<Result>();
+            } 
+            
+            try
+            {                
+                if (IsOneWayConversion(query.RawQuery) 
+                    || IsTwoWayConversion(query.RawQuery))
+                {
+                    var _money = Convert.ToDecimal(query.FirstSearch);
+                    var _fromCurrency = query.RawQuery.Split(' ')[1].ToUpper();
+                    var _toCurrency = settings.BaseCurrency.ToUpper();
 
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-            var response = (HttpWebResponse)request.GetResponse();
-            using (new StreamReader(response.GetResponseStream()))
+                    if (IsTwoWayConversion(query.RawQuery))
+                    {
+                        _toCurrency = query.RawQuery.Split(' ')[3].ToUpper();
+                    }
+
+                    if (!Enum.IsDefined(
+                           typeof(RateList),
+                           _toCurrency))
+                        return new List<Result>();
+
+                    var item = new CacheItem()
+                    {
+                        From = _fromCurrency,
+                        To = _toCurrency,
+                        Created = DateTime.Now.Date
+                    };
+
+                    // checking of query is in cache
+                    if (cache.Count > 0
+                        && cache.Any(i => i.From == item.From
+                            && i.To == item.To
+                            && i.Created.Date == item.Created.Date))
+                    {
+                        item.Rate = cache
+                            .Where(i => i.From == item.From
+                                && i.To == item.To
+                                && i.Created.Date == item.Created.Date)
+                            .Select(x => x.Rate)
+                            .FirstOrDefault();
+                    }
+                    else
+                    {                        
+                        try
+                        {
+                            if(settings.ActiveApiProvider == "frankfurter.app") 
+                            {
+                                ServicePointManager.Expect100Continue = true;
+                                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                                var url = $"https://api.frankfurter.app/latest?from={item.From}&to={item.To}";
+                                var request = (HttpWebRequest)WebRequest.Create(url);
+                                request.Method = "GET";
+                                var response = (HttpWebResponse)request.GetResponse();
+                                using (new StreamReader(response.GetResponseStream()))
+                                {
+                                    var responsestring = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                                    var currency = JsonConvert.DeserializeObject<Models.Currency>(responsestring);
+                                    item.Rate = currency.GetRate(item.To);
+                                    item.Created = DateTime.Parse(currency.date);
+                                }
+                                cache.Add(item);
+                                
+                            }                            
+                        }
+                        catch (Exception e)
+                        {
+                            new Result
+                            {
+                                Title = $"Failed to load data from API, {e.Message}",
+                                IcoPath = "Images/bank.png",
+                                Score = 10
+                            };
+                        }
+                        
+
+                    }
+                    var amount = ((double)(_money * item.Rate)).ToString();
+                    return new List<Result>()
+                    {
+                        new Result
+                        {
+                            Title = $"{_money} {_fromCurrency} = {amount} {_toCurrency}",
+                            IcoPath = "Images/bank.png",
+                            SubTitle = $"Enter to copy. Source: https://frankfurter.app (Updated {item.Created.ToString("yyyy-MM-dd")})",
+                            Score = 100,
+                            Action = c =>
+                            {
+                                try
+                                {
+                                    Clipboard.SetText(amount);
+                                    return true;
+                                }
+                                catch (ExternalException)
+                                {
+                                    MessageBox.Show("Copy failed, please try later");
+                                    return false;
+                                }
+                            }
+                        }
+                    };
+                }
+            }
+            catch
             {
-                var responsestring = new StreamReader(response.GetResponseStream()).ReadToEnd();
-
-                var currency =  JsonConvert.DeserializeObject<Models.Currency>(responsestring);
-
-                _cache.Add(searchParameters,currency);
-                return currency;
+                // Ignored
             }
+            return new List<Result>();
         }
 
+        public bool IsOneWayConversion(string query)
+        {
+            return (Regex.IsMatch(query, @"^(\d+(?:[.,]\d+)?)?\s([A-Za-z]{3})$")); //10,2 usd
+        }
+
+        public bool IsTwoWayConversion(string query)
+        {
+            return (
+                Regex.IsMatch(query, @"^(\d+(?:[.,]\d+)?)?\s([A-Za-z]{3})\s([i][n])\s([A-Za-z]{3})") // 10,2 usd in nok 
+                || Regex.IsMatch(query, @"^(\d+(?:[.,]\d+)?)?\s([A-Za-z]{3})\s([A-Za-z]{3})")); // 10,2 usd nok
+        }
+
+        public Control CreateSettingPanel()
+        {
+            return new CurrencySettings(viewModel);
+        }
+
+        public void Save()
+        {
+            viewModel.Save();
+        }
     }
 }
---
+++ src/Wox.Plugin.Currency/Models/CacheItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wox.Plugin.Currency.Models
+{
+    internal class CacheItem
+    {
+        public DateTime Created { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
